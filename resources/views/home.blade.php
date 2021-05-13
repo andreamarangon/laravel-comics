c6eca74f08843f2db1c07e5816be15e4c2b88d81
Andrea Marangon <marangonandrea84@gmail.com>
2021-05-13 17:40:53 +0200

    lavoro su single(detail)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,6 +18,27 @@
   <div class="my-btn-main">load more</div>
 </section>
 <section class="shop">
-
+  <div class="container-shop">
+    <div class="card-shop">
+      <img src="/img/buy-comics-digital-comics.png" alt="">
+      <span class="text-uppercase">digital comics</span>
+    </div>
+    <div class="card-shop">
+      <img src="/img/buy-comics-merchandise.png" alt="">
+      <span class="text-uppercase">dc merchandise</span>
+    </div>
+    <div class="card-shop">
+      <img src="/img/buy-comics-subscriptions.png" alt="">
+      <span class="text-uppercase">subscription</span>
+    </div>
+    <div class="card-shop">
+      <img src="/img/buy-comics-shop-locator.png" alt="">
+      <span class="text-uppercase">comic shop locator</span>
+    </div>
+    <div class="card-shop">
+      <img src="/img/buy-dc-power-visa.svg" alt="">
+      <span class="text-uppercase">dc power visa</span>
+    </div>
+  </div>
 </section>
 @endsection
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -8,9 +8,17 @@
  <section class="detail-center">
    <div class="detail-center-left">
      <h1>{{ $comic['title'] }}</h1>
-
+     <div class="container-green">
+      <div class="container-green-left">
+        <span>U.S. Price:{{ $comic['price'] }}</span>
+        <span class="text-uppercase">available</span>
+      </div>
+      <div class="container-green-right">Check Availability<i class="fas fa-sort-down"></i></div>
+    </div>
+    <p>{{ $comic['description'] }}</p>
    </div>
-   <div class="detail-center-left">
+   <div class="detail-center-right">
+
    </div>
  </section>
  <section class="detail-down">
