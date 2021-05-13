c57fdc2c2afd5d087930f3e64a4bbc4cf360cdb0
Andrea Marangon <marangonandrea84@gmail.com>
2021-05-13 18:13:12 +0200

    avanzamento su single

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -8,15 +8,15 @@
  </section>
  <section class="detail-center pb-10">
    <div class="detail-center-left pt-10">
-     <h1>{{ $comic['title'] }}</h1>
-     <div class="container-green">
+     <h1 class="text-uppercase">{{ $comic['title'] }}</h1>
+     <div class="container-green mt-3">
       <div class="container-green-left">
         <span>U.S. Price:{{ $comic['price'] }}</span>
         <span class="text-uppercase">available</span>
       </div>
       <div class="container-green-right">Check Availability<i class="fas fa-sort-down"></i></div>
     </div>
-    <p>{{ $comic['description'] }}</p>
+    <p class="mt-2">{{ $comic['description'] }}</p>
    </div>
    <div class="detail-center-right pt-8">
     <span class="text-uppercase">advertisement</span>
@@ -25,7 +25,12 @@
  </section>
  <section class="detail-down">
    <div class="container-detail-down">
+    <div class="container-detail-down-left">
 
+    </div>
+    <div class="container-detail-down-right">
+
+    </div>
    </div>
 
  </section>
