6b4b9b6750c49587322db49064523f69641aae9c
Andrea Marangon <marangonandrea84@gmail.com>
2021-05-13 15:30:45 +0200

    header terminata-lavoro su main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,22 @@
 @extends('layouts.app')
 
 @section('main')
-<div class="container-card">
-  <!-- ciclo la card con un foreach-->
-  @foreach ($comics as $index => $comic)
-    <div class="card">
-      <a href="{{route('detail', ['id' => $index])}}">
-        <div class="container-img" style="background-image: url('{{ $comic['thumb'] }}')" >
-        </div>
-        <h5 class="text-uppercase">{{ $comic['series'] }}</h5>
-      </a>
-    </div>
-  @endforeach
-</div>
+<section class="comics-section pb-2">
+  <div class="container-card pt-6">
+    <!-- ciclo la card con un foreach-->
+    @foreach ($comics as $index => $comic)
+      <div class="card">
+        <a href="{{route('detail', ['id' => $index])}}">
+          <div class="container-img" style="background-image: url('{{ $comic['thumb'] }}')" >
+          </div>
+          <h5 class="text-uppercase mt-2">{{ $comic['series'] }}</h5>
+        </a>
+      </div>
+    @endforeach
+  </div>
+  <div class="my-btn-main">load more</div>
+</section>
+<section class="shop">
+
+</section>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,20 @@
 <header>
   <nav>
-    <div class="nav-up"></div>
+    <div class="nav-up">
+      <div class="container-nav-up">
+        <ul>
+          <li class="text-uppercase">dc power visa &reg;</li>
+          <li class="text-uppercase">additional dc sites<i class="fas fa-sort-down"></i></li>
+        </ul>
+      </div>
+    </div>
     <div class="nav-down">
       <div class="img-logo-dc">
         <img src="./img/dc-logo.png" alt="">
       </div>
       <ul>
         <li class="text-uppercase"><a href="">characters</a></li>
-        <li class="text-uppercase"><a href="">comics</a></li>
+        <li class="text-uppercase"><a class="active" href="">comics</a></li>
         <li class="text-uppercase"><a href="">movies</a></li>
         <li class="text-uppercase"><a href="">tv</a></li>
         <li class="text-uppercase"><a href="">games</a></li>
@@ -21,6 +28,5 @@
     </div>
   </nav>
   <div class="jumbo">
-
   </div>
 </header>
